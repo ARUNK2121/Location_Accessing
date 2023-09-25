359a214a15031803b0642bad54397511f4642221
Arun K <arthurbishop120@gmail.com>
2023-09-25 11:51:59 +0530

    changed to gin

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.New()
+
+	engine.LoadHTMLGlob("templates/*.html")
+
+	engine.Use(gin.Logger())
+
+	engine.GET("/", indexHandler)
+	engine.GET("/location", DisplayLocation)
+}
+
+func DisplayLocation(c *gin.Context) {
+
+}
+
+func indexHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", nil)
+}
